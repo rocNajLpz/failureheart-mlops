3612f9bda7db3d0aa59fb89e1ea14bb546f47c98
rdnajera <rocio.najera@oracle.com>
2024-05-07 12:46:08 -0600

    adding training and evaluation

--- src/stages/data_prepare.py
+++ src/stages/data_prepare.py
@@ -6,7 +6,7 @@ from src.utils.logs import get_logger
 
 
 def data_load(config_path: Text) -> None:
-    """Load raw data.
+    """Cargar datos desde el csv.
     Args:
         config_path {Text}: path to config
     """
@@ -17,12 +17,14 @@ def data_load(config_path: Text) -> None:
     logger = get_logger('DATA_LOAD', log_level=config['base']['log_level'])
 
     logger.info('Get dataset')
+
     # Load the dataset
     df = pd.read_csv(config['data_load']['dataset_csv'])
 
     # drop NaNs
     df = df.dropna(axis=0, how='any')
 
+    # save data from original csv to newone
     logger.info('Save processed data')
     df.to_csv(config['data_load']['dataset_prepare'], index=False)
 
